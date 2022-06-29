f9359f39c65455df8de4af32d547d8415af3d284
momela <omelaymy@gmail.com>
2022-06-29 23:25:29 +0300

    work todoapp with panel admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'date', 'done')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'description')
+    list_editable = ('done', )
+    list_filter = ('done', )
